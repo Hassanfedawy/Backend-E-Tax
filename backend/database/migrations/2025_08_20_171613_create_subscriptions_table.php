5f4f261ea4d4ac2819f2584e82b3a3e8ffca25c4
HanaAkabawy <hanaakabawy123@gmail.com>
2025-08-21 11:09:49 -0700

    final changes into the migration files

--- backend/database/migrations/2025_08_20_171613_create_subscriptions_table.php
+++ backend/database/migrations/2025_08_20_171613_create_subscriptions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
            
-            $table->string('Name');   
+            $table->string('name');   
             $table->integer('no_of_posts'); 
            
             $table->float('cost');
