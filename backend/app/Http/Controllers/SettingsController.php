b73efb9a3c820ed60408ee884bdcda1818f31282
HanaAkabawy <hanaakabawy123@gmail.com>
2025-08-26 23:44:20 -0700

    final changes in the settings module

--- backend/app/Http/Controllers/SettingsController.php
+++ backend/app/Http/Controllers/SettingsController.php
@@ -27,8 +27,8 @@ class SettingsController extends Controller
     /**
  * Update an existing setting by ID.
  */
-public function update(Request $request)
-{   $id=auth()->id();
+public function update(Request $request, $id)
+{   
     $request->validate([
         'key'   => 'sometimes|string|unique:settings,key,' . $id,
         'value' => 'required|string',
@@ -103,8 +103,8 @@ public function getRevenueBySubscription()
     /**
      * Delete a setting by id
      */
-    public function destroy()
-    {   $id=auth()->id();
+    public function destroy($id)
+    {   
         $setting = Setting::find($id);
 
         if (!$setting) {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -130,8 +130,8 @@ Route::get('/stats/posts-today', [StatsController::class, 'getPostsToday']);
 
 Route::post('/settings', [SettingsController::class, 'store']);
 Route::put('/settings/{key}', [SettingsController::class, 'update']);
-Route::get('/profile/{id}', [ProfileController::class, 'show']);
-Route::put('/profile/{id}', [ProfileController::class, 'update']);
+Route::get('/profile', [ProfileController::class, 'show']);
+Route::put('/profile', [ProfileController::class, 'update']);
 Route::get('/settings', [SettingsController::class, 'index']);   
 Route::delete('/settings/{id}', [SettingsController::class, 'destroy']); 
 
